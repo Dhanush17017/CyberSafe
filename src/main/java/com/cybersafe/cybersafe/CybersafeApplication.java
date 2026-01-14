41542f071c05912032c4b274148d5adfbd94827f
Dhanush <154488483+Dhanush17017@users.noreply.github.com>
2026-01-14 14:58:06 +0530

    Added java files

---
+++ src/main/java/com/cybersafe/cybersafe/CybersafeApplication.java
@@ -0,0 +1,14 @@
+package com.cybersafe.cybersafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersafeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CybersafeApplication.class, args);
+	}
+
+}
